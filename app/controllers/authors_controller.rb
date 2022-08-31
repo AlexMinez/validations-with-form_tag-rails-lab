3c2da982a4f93808ff7a03ad34f0879262240d7c
AlexMinez <alex_minihan@outlook.com>
2022-08-31 13:47:30 +1000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,12 +7,28 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+  def edit
+    @author = Author.find(params[:id])
   end
 
+  def create
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+    redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+def update
+    @post.update(author_params)
+    if @post.valid?
+      @post.update(author_params)
+        redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
   private
 
   def author_params
